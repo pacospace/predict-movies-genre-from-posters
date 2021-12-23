07d94f384166b127232b004e932eff5f88259979
Francesco Murdaca <fmurdaca@redhat.com>
2021-12-23 12:13:07 +0100

    adjust docstrings
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- src/data/data_handler.py
+++ src/data/data_handler.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Processing code."""
+"""Data Handler code."""
 
 import pandas as pd
 
